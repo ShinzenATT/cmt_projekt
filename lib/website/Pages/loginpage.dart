e23cf863c14a12a3a1101183b7bc246c74153f4c
JohannaJH <johannaj@student.chalmers.se>
2021-11-09 19:24:45 +0100

    Första versionen av loginpage för hemsidan klar.

--- lib/website/Pages/loginpage.dart
+++ lib/website/Pages/loginpage.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+///First version of loginpage for the website.
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -8,10 +12,122 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String title = 'Comment'; //The website logotype.
+  bool _passwordVisible = false; // Controlls the hide-password feature.
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                title,
+                style:
+                    const TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40),
+                ),
+                elevation: 3,
+                child: Container(
+                  padding: const EdgeInsets.all(30),
+                  width: 400,
+                  height: 850,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Välkommen',
+                            style: TextStyle(
+                                fontSize: 30, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                              labelText: 'E-post eller telefonnummer',
+                            ),
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Lösenord',
+                              suffixIcon: IconButton(
+                                hoverColor: Colors.transparent,
+                                splashRadius: null,
+                                splashColor: Colors.transparent,
+                                icon: Icon(
+                                  // Based on passwordVisible state choose the icon
+                                  _passwordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                  color: Theme.of(context).primaryColorDark,
+                                ),
+                                onPressed: () {
+                                  // Update the state i.e. toogle the state of passwordVisible variable
+                                  setState(() {
+                                    _passwordVisible = !_passwordVisible;
+                                  });
+                                },
+                              ),
+                            ),
+                            obscureText: !_passwordVisible,
+                          ),
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: TextButton(
+                              onPressed: () {},
+                              child: const Text('Glömt lösenord?'),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Logga in'),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'Gästläge',
+                              style: TextStyle(fontStyle: FontStyle.italic),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child:
+                                const Text('Inget konto? Registrera dig här'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
