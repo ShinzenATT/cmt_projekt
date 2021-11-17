2a133a315fdcfe3c775407c8bf74bd31982defa5
JohannaJH <johannaj@student.chalmers.se>
2021-11-17 15:03:32 +0100

    Skapat skal till framtida meny för hemsidan.

--- lib/website/View/web_homepage.dart
+++ lib/website/View/web_homepage.dart
@@ -12,7 +12,43 @@ class _WebHomePageState extends State<WebHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Text("hej"),
+      body: ElevatedButton(
+        child: Text('knapp'),
+        onPressed: () {},
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.green,
+              ),
+              child: Text('MENY'),
+            ),
+            ListTile(
+              title: const Text('Profil information'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Inställningar'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
