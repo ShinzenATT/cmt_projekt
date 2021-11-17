08973586d5f163cc02d7a437da470c504f993d76
OveSundberg <sam00@telia.com>
2021-11-17 13:42:45 +0100

    Initial websocket connection established

--- lib/server/streamclient.dart
+++ lib/server/streamclient.dart
@@ -2,5 +2,9 @@ import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() {
   WebSocketChannel client =
-      WebSocketChannel.connect(Uri.parse("ws://10.0.178.155:5605"));
+       WebSocketChannel.connect(Uri.parse("ws://localhost:8080"));
+  client.sink.add('Simon'.codeUnits);
+  client.stream.listen((event) {
+    print(event);
+  });
 }
--- lib/server/streamserver.dart
+++ lib/server/streamserver.dart
@@ -1,10 +1,17 @@
 import 'dart:io';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_web_socket/shelf_web_socket.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() async {
-  ServerSocket server = await ServerSocket.bind("10.0.178.155", 5605);
-  print(InternetAddress.anyIPv4);
-  HttpServer httpserver = HttpServer.listenOn(server);
-  server.listen((event) {
-    print(event);
+  var handler = webSocketHandler((webSocket) {
+    webSocket.stream.listen((message) {
+      webSocket.sink.add("echo $message");
+      print(message.runtimeType);
+    });
+  });
+
+  shelf_io.serve(handler, 'localhost', 8080).then((server) {
+    print('Serving at ws://${server.address.host}:${server.port}');
   });
 }
