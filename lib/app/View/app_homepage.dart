b56b52e17cc64fe8b335a48f23d9b43881734eef
Keppan <kevin.willy@hotmail.com>
2021-12-02 21:23:08 +0100

    Har gjort om homepage utifrån Johannas tema.

--- lib/app/View/app_homepage.dart
+++ lib/app/View/app_homepage.dart
@@ -2,7 +2,9 @@ import 'dart:async';
 import 'dart:typed_data';
 
 import 'package:cmt_projekt/server/streamclient.dart';
+import 'package:cmt_projekt/viewmodel/loginpageviewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/src/provider.dart';
 import 'package:sound_stream/sound_stream.dart';
 
 class AppHomePage extends StatefulWidget {
@@ -13,70 +15,40 @@ class AppHomePage extends StatefulWidget {
 }
 
 class _AppHomePageState extends State<AppHomePage> {
-  RecorderStream _recorder = RecorderStream();
-  PlayerStream _player = PlayerStream();
-
- //Client c = Client();
-
-  List<Uint8List> _micChunks = [];
-  bool _isRecording = false;
-  bool _isPlaying = false;
-
-  late StreamSubscription _recorderStatus;
-  late StreamSubscription _playerStatus;
-  late StreamSubscription _audioStream;
-
-  @override
-  void initState() {
-    super.initState();
-    initPlugin();
-  }
-
-  @override
-  void dispose() {
-    _recorderStatus.cancel();
-    _playerStatus.cancel();
-    _audioStream.cancel();
-    super.dispose();
-  }
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(80.0),
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                      Colors.black,
+                      Colors.blueAccent,
+                    ])),
+          ),
+          elevation: 0,
+          centerTitle: true,
+          title: Column(
+            children: [
+              Text(
+                context.read<LoginPageViewModel>().title.toUpperCase(),
+                style:
+                const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
+              const Text(
+                'Din moderna radioapp',
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
       ),
-      body: const Text("apphome yay"),
+      body: ElevatedButton(child: Text("hej"),onPressed: null,),
     );
-  }
-
-  Future<void> initPlugin() async {
-    _recorderStatus = _recorder.status.listen((status) {
-      if (mounted)
-        setState(() {
-          _isRecording = status == SoundStreamStatus.Playing;
-        });
-    });
-
-    _audioStream = _recorder.audioStream.listen((data) {
-      if (true) {
-        //_player.writeChunk(data);
-     //   c.client.sink.add(data);
-      } else {
-        _micChunks.add(data);
-      }
-    });
-
-    _playerStatus = _player.status.listen((status) {
-      if (mounted)
-        setState(() {
-          _isPlaying = status == SoundStreamStatus.Playing;
-        });
-    });
-
-    await Future.wait([
-      _recorder.initialize(),
-      _player.initialize(),
-    ]);
   }
 }
