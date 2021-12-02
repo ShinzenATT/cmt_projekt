08fcddc20a91366a07fe57b7dbb0b2fb4a43e83e
JohannaJH <johanna.hallberg1130@gmail.com>
2021-12-02 21:48:41 +0100

    Ändrat design på demopage

--- lib/app/View/app_demo.dart
+++ lib/app/View/app_demo.dart
@@ -34,48 +34,58 @@ class StreamLoop extends StatefulWidget {
 }
 
 class _StreamLoopState extends State<StreamLoop> {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(
-
-          ),
-          body: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.all(3),
-                padding: const EdgeInsets.all(3),
-                height: 80,
-                width: double.infinity,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Color(0xFFFAF0E6),
-                  border: Border.all(
-                    color: Colors.indigo,
-                    width: 3,
-                  ),
-                ),
-                child: Row(children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      context.read<StreamViewModel>().getRecFn();
-                    },
-                    //color: Colors.white,
-                    //disabledColor: Colors.grey,
-                    child: Text(context.watch<StreamViewModel>().smodel.recorder!.isRecording ? 'Stop' : 'Record'),
-                  ),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Text(context.watch<StreamViewModel>().smodel.recorder!.isRecording
-                      ? 'Playback to your headset!'
-                      : 'Recorder is stopped'),
-                ]),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Column(
+          children: [Text('COMMENT'), Text('Demo')],
+        ),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  colors: [
+                Colors.black,
+                Colors.blueAccent,
+              ])),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: const EdgeInsets.all(3),
+            padding: const EdgeInsets.all(3),
+            height: 80,
+            width: double.infinity,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Color(0xFFFAF0E6),
+              border: Border.all(
+                color: Colors.indigo,
+                width: 3,
               ),
-            ],
+            ),
+            child: Text(
+                context.watch<StreamViewModel>().smodel.recorder!.isRecording
+                    ? 'Playback to your headset!'
+                    : 'Recorder is stopped'),
           ),
-        ));
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+            context.watch<StreamViewModel>().smodel.recorder!.isRecording
+                ? Icons.mic
+                : Icons.mic_off),
+        onPressed: () {
+          context.read<StreamViewModel>().getRecFn();
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    ));
   }
 }
