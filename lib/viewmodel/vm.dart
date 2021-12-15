22b8b9897f970e67a0fed9acf9b031afe3e78722
OveSundberg <sam00@telia.com>
2021-12-15 15:59:36 +0100

    Works just fine.

--- lib/viewmodel/vm.dart
+++ lib/viewmodel/vm.dart
@@ -37,7 +37,7 @@ class VM with ChangeNotifier {
 
   TextEditingController get channelName => lm.channelName;
 
-  void setChannelSettings(context) {
+  void setChannelSettings() {
     print(channelName.value.text);
     print(getCategory);
     Prefs().storedData.setString("channelName", channelName.value.text);
