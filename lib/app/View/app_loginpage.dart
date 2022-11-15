df86e588f7c6f027ec002c43886f7b36b72535cb
AlexanderBratic <98606025+AlexanderBratic@users.noreply.github.com>
2022-11-15 20:55:18 +0100

    Added flex to widgets in loginpage and create account. Also added textInputAction to the text fields

--- lib/app/View/app_loginpage.dart
+++ lib/app/View/app_loginpage.dart
@@ -49,149 +49,196 @@ class AppLoginPage extends StatelessWidget {
         height: MediaQuery.of(context).size.height,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'Välkommen',
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-                const Text(
-                  'Vänligen ange dina inloggningsuppgifter',
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                TextFormField(
-                  controller: context.watch<VM>().login,
-                  decoration: const InputDecoration(
-                    labelText: 'E-post eller telefonnummer',
-                  ),
-                ),
-                TextFormField(
-                  controller: context.watch<VM>().password,
-                  decoration: InputDecoration(
-                    labelText: 'Lösenord',
-                    suffixIcon: IconButton(
-                      hoverColor: Colors.transparent,
-                      splashRadius: null,
-                      splashColor: Colors.transparent,
-                      icon: Icon(
-                        // Based on passwordVisible state choose the icon
-                        context.read<VM>().passwordVisibilityLogin
-                            ? Icons.visibility
-                            : Icons.visibility_off,
-                        color: Theme.of(context).primaryColorDark,
-                      ),
-                      onPressed: () {
-                        // Update the state i.e. toogle the state of passwordVisible variable
-                        context.read<VM>().changePasswordVisibilityLogin();
-                      },
-                    ),
-                  ),
-                  obscureText: !context.watch<VM>().passwordVisibilityLogin,
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: TextButton(
-                    onPressed: () {
-                      /// Implement password reset
-                    },
-                    child: const Text('Glömt ditt lösenord?'),
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                SizedBox(
-                  width: 200,
-                  height: 50,
-                  child: GradientElevatedButton(
-                    child: const Text(
-                      'Logga in',
-                      style: TextStyle(
-                        fontSize: 25,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+          children: [//All children are wrapped in flexible to adjust when keyboard is used
+            Flexible(
+                flex: 3,
+                child: Column(
+                  //mainAxisAlignment: MainAxisAlignment.center,
+                  children: [//All children are wrapped in flexible to adjust when keyboard is used
+                    const Flexible(
+                      flex: 2,
+                      child: Text(
+                        'Välkommen',
+                        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                       ),
                     ),
-                    onPressed: () {
-                      context.read<VM>().loginAttempt(context);
-                    },
-                    gradient: const LinearGradient(
-                        begin: Alignment.centerLeft,
-                        end: Alignment.centerRight,
-                        colors: [
-                          Colors.greenAccent,
-                          Colors.blueAccent,
-                        ]),
-                  ),
-                ),
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Välkommen att logga in som ',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    InkWell(
-                      splashColor: Colors.transparent,
-                      highlightColor: Colors.transparent,
-                      hoverColor: Colors.transparent,
-                      onTap: () {
-                        context.read<VM>().guestSign(context);
-                      },
-                      child: const Text(
-                        "gäst",
-                        style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.blueAccent,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text(
-                        'Inget konto än? ',
+                    const Flexible(
+                      flex: 1,
+                      child: Text(
+                        'Vänligen ange dina inloggningsuppgifter',
                         style: TextStyle(
                           fontSize: 16,
                         ),
                       ),
-                      InkWell(
-                        splashColor: Colors.transparent,
-                        highlightColor: Colors.transparent,
-                        hoverColor: Colors.transparent,
-                        onTap: () {
-                          context
-                              .read<VM>()
-                              .changePage(context, constant.createAcc);
-                        },
-                        child: const Text(
-                          "Registrera dig här",
-                          style: TextStyle(
-                              fontSize: 16,
-                              color: Colors.blueAccent,
-                              fontWeight: FontWeight.bold),
+                    ),
+
+                    const Flexible(
+                      flex: 1,
+                      child: SizedBox(
+                        height: 40,
+                      ),
+                    ),
+                    Flexible(
+                      flex: 3,
+                      child: TextFormField(
+                        controller: context
+                            .watch<VM>()
+                            .login,
+                        textInputAction: TextInputAction.next, // Moves focus to next text field.
+                        decoration: const InputDecoration(
+                          labelText: 'E-post eller telefonnummer',
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                    Flexible(
+                      flex: 3,
+                      child: TextFormField(
+                        controller: context
+                            .watch<VM>()
+                            .password,
+                        textInputAction: TextInputAction.done, // Close keyboard.
+                        decoration: InputDecoration(
+                          labelText: 'Lösenord',
+                          suffixIcon: IconButton(
+                            hoverColor: Colors.transparent,
+                            splashRadius: null,
+                            splashColor: Colors.transparent,
+                            icon: Icon(
+                              // Based on passwordVisible state choose the icon
+                              context
+                                  .read<VM>()
+                                  .passwordVisibilityLogin
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Theme
+                                  .of(context)
+                                  .primaryColorDark,
+                            ),
+                            onPressed: () {
+                              // Update the state i.e. toogle the state of passwordVisible variable
+                              context.read<VM>().changePasswordVisibilityLogin();
+                            },
+                          ),
+                        ),
+                        obscureText: !context.watch<VM>().passwordVisibilityLogin,
+                      ),
+                    ),
+                    Flexible(
+                      flex: 2,
+                      child: Align(
+                        alignment: Alignment.bottomRight,
+                        child: TextButton(
+                          onPressed: () {
+                            /// Implement password reset
+                          },
+                          child: const Text('Glömt ditt lösenord?'),
+                        ),
+                      ),
+                    ),
+                    const Flexible(
+                      flex: 1,
+                      child: SizedBox(
+                        height: 30,
+                      ),
+                    ),
+                    Flexible(
+                      flex: 3,
+                      child: SizedBox(
+                        width: 200,
+                        height: 50,
+                        child: GradientElevatedButton(
+                          child: const Text(
+                            'Logga in',
+                            style: TextStyle(
+                              fontSize: 25,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          onPressed: () {
+                            context.read<VM>().loginAttempt(context);
+                          },
+                          gradient: const LinearGradient(
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                              colors: [
+                                Colors.greenAccent,
+                                Colors.blueAccent,
+                              ]),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+            ),
+            Flexible(
+                flex: 1,
+                child: Column(
+                  //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Flexible(
+                      flex: 1,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Välkommen att logga in som ',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          InkWell(
+                            splashColor: Colors.transparent,
+                            highlightColor: Colors.transparent,
+                            hoverColor: Colors.transparent,
+                            onTap: () {
+                              context.read<VM>().guestSign(context);
+                            },
+                            child: const Text(
+                              "gäst",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.blueAccent,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Flexible(
+                      flex: 1,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'Inget konto än? ',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                            InkWell(
+                              splashColor: Colors.transparent,
+                              highlightColor: Colors.transparent,
+                              hoverColor: Colors.transparent,
+                              onTap: () {
+                                context
+                                    .read<VM>()
+                                    .changePage(context, constant.createAcc);
+                              },
+                              child: const Text(
+                                "Registrera dig här",
+                                style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.blueAccent,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
             ),
           ],
         ),
