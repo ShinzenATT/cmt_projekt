41ca5ce7d5941b2a376352e2a5b362ffb248712e
shinzenatt <42674003+ShinzenATT@users.noreply.github.com>
2022-12-03 13:47:09 +0100

    added so stream/listening views react to the update messages so all data should be updates on realtime

--- lib/views/listenchannel_view.dart
+++ lib/views/listenchannel_view.dart
@@ -1,10 +1,8 @@
-import 'package:cmt_projekt/apis/prefs.dart';
 import 'package:cmt_projekt/models/query_model.dart';
 import 'package:cmt_projekt/view_models/stream_vm.dart';
 import 'package:cmt_projekt/view_models/main_vm.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 
 ///The page responsible for displaying what the viewer sees when listening to a stream.
 class AppListenPage extends StatelessWidget {
@@ -16,90 +14,49 @@ class AppListenPage extends StatelessWidget {
       context.read<StreamViewModel>().closeClient();
       return context.read<MainViewModel>().willPopCallback();
     }
+
     return WillPopScope(
       onWillPop: willPopCallback,
       child: Scaffold(
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(80.0),
-          child: AppBar(
-            flexibleSpace: Container(
-              decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                      colors: [
-                    Colors.greenAccent,
-                    Colors.blueAccent,
-                  ])),
-            ),
-            elevation: 0,
-            centerTitle: true,
-            title: Column(
-              children: [
-                Text(
-                  context.read<MainViewModel>().title.toUpperCase(),
-                  style: const TextStyle(
-                      fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-                const Text(
-                  'Din moderna radioapp',
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-                )
-              ],
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(80.0),
+            child: AppBar(
+              flexibleSpace: Container(
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                        colors: [
+                      Colors.greenAccent,
+                      Colors.blueAccent,
+                    ])),
+              ),
+              elevation: 0,
+              centerTitle: true,
+              title: Column(
+                children: [
+                  Text(
+                    context.read<MainViewModel>().title.toUpperCase(),
+                    style: const TextStyle(
+                        fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                  const Text(
+                    'Din moderna radioapp',
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
             ),
           ),
-        ),
-        body: Container(
-          color: Colors.black,
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Expanded(
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Text(
-                          (Prefs().storedData.getString("channelName")!),
-                          style: const TextStyle(
-                            color: Colors.greenAccent,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                    Text(
-                      (Prefs().storedData.getString("hostUsername")!),
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                child: Row(
-                  children: [
-                    buildStream(context),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+          body: buildStream(context)),
     );
   }
 
   Widget buildStream(BuildContext context) {
     return StreamBuilder(
-      stream: context.watch<MainViewModel>().databaseAPI.channelController.stream,
-      initialData: 0,
+      stream: context.watch<StreamViewModel>().smodel.c!.msgController.stream,
+      initialData:
+          QueryModel.fromJson({"total": 0, "channelname": "", "usename": ""}),
       builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(child: CircularProgressIndicator());
@@ -108,79 +65,129 @@ class AppListenPage extends StatelessWidget {
           if (snapshot.hasError) {
             return const Text("error");
           } else if (snapshot.hasData) {
-            QueryModel? channel = context.read<MainViewModel>().getChannel(
-                snapshot.data, Prefs().storedData.getString("channelName")!);
-            return Flexible(
-                fit: FlexFit.loose,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Row(
-                        children: [
-                          const Icon(
-                            Icons.person,
-                            color: Colors.white,
-                          ),
-                          Text(
-                            channel != null ? channel.total.toString() : "0",
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                            ),
-                          ),
-                          const Text(
-                            ' lyssnare',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Container(
-                        margin:
-                            const EdgeInsets.only(left: 3, right: 3, top: 100),
-                        height: MediaQuery.of(context).size.height * 0.4,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: Colors.black,
-                          border: Border.all(
-                            color: Colors.white30,
-                            width: 3,
-                          ),
-                        ),
-                        child: Column(
-                          children: [
-                            Icon(
-                              Icons.play_arrow_outlined,
-                              color: Colors.white,
-                              size: MediaQuery.of(context).size.height * 0.2,
-                            ),
-                            Divider(
-                              thickness: 1,
-                              color: Colors.white,
-                              indent: MediaQuery.of(context).size.width * 0.05,
-                              endIndent:
-                                  MediaQuery.of(context).size.width * 0.05,
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Text(
-                                "No information",
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.white,
-                                ),
+            QueryModel channel = snapshot.data;
+            return Container(
+              color: Colors.black,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Text(
+                              channel.channelname as String,
+                              style: const TextStyle(
+                                color: Colors.greenAccent,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
-                          ],
+                          ),
                         ),
-                      ),
-                    ],
+                        Text(
+                          channel.username as String,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ));
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                    child: Row(
+                      children: [
+                        Flexible(
+                            fit: FlexFit.loose,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Row(
+                                    children: [
+                                      const Icon(
+                                        Icons.person,
+                                        color: Colors.white,
+                                      ),
+                                      Text(
+                                        channel.total.toString(),
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 18,
+                                        ),
+                                      ),
+                                      const Text(
+                                        ' lyssnare',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 18,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  Container(
+                                    margin: const EdgeInsets.only(
+                                        left: 3, right: 3, top: 100),
+                                    height: MediaQuery.of(context).size.height *
+                                        0.4,
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                      color: Colors.black,
+                                      border: Border.all(
+                                        color: Colors.white30,
+                                        width: 3,
+                                      ),
+                                    ),
+                                    child: Column(
+                                      children: [
+                                        Icon(
+                                          Icons.play_arrow_outlined,
+                                          color: Colors.white,
+                                          size: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
+                                              0.2,
+                                        ),
+                                        Divider(
+                                          thickness: 1,
+                                          color: Colors.white,
+                                          indent: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.05,
+                                          endIndent: MediaQuery.of(context)
+                                                  .size
+                                                  .width *
+                                              0.05,
+                                        ),
+                                        const Padding(
+                                          padding: EdgeInsets.all(8.0),
+                                          child: Text(
+                                            "No information",
+                                            style: TextStyle(
+                                              fontSize: 20,
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            );
           } else {
             return const Text("No active channels");
           }
