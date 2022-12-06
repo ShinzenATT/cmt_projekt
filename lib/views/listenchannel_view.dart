44bfa0267c8c0fe8c45b3a196c687be05a5af32d
Anaemix <henning@gmail.com>
2022-12-06 16:25:21 +0100

    updated listening view, images and description still needs to be connected though

--- lib/views/listenchannel_view.dart
+++ lib/views/listenchannel_view.dart
@@ -2,7 +2,9 @@ import 'package:cmt_projekt/models/query_model.dart';
 import 'package:cmt_projekt/view_models/stream_vm.dart';
 import 'package:cmt_projekt/view_models/main_vm.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
+import 'dart:ui';
 
 ///The page responsible for displaying what the viewer sees when listening to a stream.
 class AppListenPage extends StatelessWidget {
@@ -53,6 +55,9 @@ class AppListenPage extends StatelessWidget {
   }
 
   Widget buildStream(BuildContext context) {
+    const streamImage = "https://images.unsplash.com/photo-1633876841461-772d2b0b0e39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1062&q=80";
+    const profileImage = "https://images.unsplash.com/photo-1582750433449-648ed127bb54?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80";
+    const streamDescription = "Jakten på lycka";
     return StreamBuilder(
       stream: context.watch<StreamViewModel>().smodel.streamClient!.msgController.stream,
       initialData:
@@ -67,126 +72,145 @@ class AppListenPage extends StatelessWidget {
           } else if (snapshot.hasData) {
             QueryModel channel = snapshot.data;
             return Container(
-              color: Colors.black,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
-                            child: Text(
-                              channel.channelname as String,
-                              style: const TextStyle(
-                                color: Colors.greenAccent,
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Text(
-                          channel.username as String,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-                    child: Row(
-                      children: [
-                        Flexible(
-                            fit: FlexFit.loose,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Row(
-                                    children: [
-                                      const Icon(
-                                        Icons.person,
-                                        color: Colors.white,
-                                      ),
-                                      Text(
-                                        channel.total.toString(),
-                                        style: const TextStyle(
+              decoration: const BoxDecoration(
+                image:  DecorationImage(
+                  image:  NetworkImage(streamImage),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.white.withOpacity(0.0)),
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                        child: Row(
+                          children: [
+                            Flexible(
+                              fit: FlexFit.loose,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    // v information about listeners
+                                    Row(
+                                      children: [
+                                        const Icon(
+                                          Icons.person,
                                           color: Colors.white,
-                                          fontSize: 18,
                                         ),
-                                      ),
-                                      const Text(
-                                        ' lyssnare',
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 18,
+                                        Text(
+                                          channel.total.toString(),
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 18,
+                                          ),
                                         ),
-                                      ),
-                                    ],
-                                  ),
-                                  Container(
-                                    margin: const EdgeInsets.only(
-                                        left: 3, right: 3, top: 100),
-                                    height: MediaQuery.of(context).size.height *
-                                        0.4,
-                                    alignment: Alignment.center,
-                                    decoration: BoxDecoration(
-                                      color: Colors.black,
-                                      border: Border.all(
-                                        color: Colors.white30,
-                                        width: 3,
+                                        const Text(
+                                          ' lyssnare',
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    //------------------------------------------
+                                    // v Central image of the stream, this should come from the tableau
+                                    //------------------------------------------
+                                    Container(
+                                      margin: const EdgeInsets.only(left: 3, right: 3, top: 100),
+                                      height: MediaQuery.of(context).size.height*0.4,
+                                      alignment: Alignment.center,
+                                      decoration:  BoxDecoration(
+                                        image: const DecorationImage(
+                                          image: NetworkImage(streamImage),
+                                          fit: BoxFit.cover,
+                                        ),
+                                        border: Border.all(
+                                          color: Colors.grey,
+                                          width: 2,
+                                        ),
                                       ),
                                     ),
-                                    child: Column(
-                                      children: [
-                                        Icon(
-                                          Icons.play_arrow_outlined,
+                                    //------------------------------------------
+                                    // v Channel name text goes here
+                                    //------------------------------------------
+                                    Text(
+                                      channel.channelname as String,
+                                      style: const TextStyle(
                                           color: Colors.white,
-                                          size: MediaQuery.of(context)
-                                                  .size
-                                                  .height *
-                                              0.2,
-                                        ),
-                                        Divider(
-                                          thickness: 1,
+                                          fontSize: 30,
+                                          shadows: [
+                                            Shadow(
+                                                offset: Offset(-1.5, -1.5),
+                                                color: Colors.black,
+                                                blurRadius: 2
+                                            ),
+                                          ]
+                                      ),
+                                    ),
+                                    const Divider(
+                                      color: Colors.grey,
+                                      thickness: 2,
+                                    ),
+                                    //------------------------------------------
+                                    //v Description for the current stream, this should come from the tableau
+                                    //------------------------------------------
+                                    const Text(
+                                      streamDescription,
+                                      style: TextStyle(
                                           color: Colors.white,
-                                          indent: MediaQuery.of(context)
-                                                  .size
-                                                  .width *
-                                              0.05,
-                                          endIndent: MediaQuery.of(context)
-                                                  .size
-                                                  .width *
-                                              0.05,
-                                        ),
-                                        const Padding(
-                                          padding: EdgeInsets.all(8.0),
-                                          child: Text(
-                                            "No information",
-                                            style: TextStyle(
-                                              fontSize: 20,
-                                              color: Colors.white,
+                                          fontSize: 15,
+                                          shadows: [
+                                            Shadow(
+                                                offset: Offset(-1.5, -1.5),
+                                                color: Colors.black,
+                                                blurRadius: 2
+                                            ),
+                                          ]
+                                      ),
+                                    ),
+                                    //------------------------------------------
+                                    //v channel's profile image is created here
+                                    //------------------------------------------
+                                    Row(
+                                      children: <Widget>[
+                                        const Spacer(),
+                                        Container(
+                                          margin: const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 20),
+                                          height: MediaQuery.of(context).size.height*0.12,
+                                          width: MediaQuery.of(context).size.height*0.12,
+                                          alignment: Alignment.topRight,
+                                          decoration: const BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            image: DecorationImage(
+                                              image: NetworkImage(profileImage),
+                                              fit: BoxFit.cover,
                                             ),
                                           ),
                                         ),
                                       ],
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
-                            ))
-                      ],
-                    ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
                   ),
-                ],
-              ),
+
+
+                ),
+              )
+              /*
+
+              */
             );
           } else {
             return const Text("No active channels");
@@ -196,5 +220,24 @@ class AppListenPage extends StatelessWidget {
         }
       },
     );
+  }
+}
+
+class ListenView extends StatefulWidget {
+  const ListenView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<ListenView> createState() => _ListenViewState();
+}
+
+class _ListenViewState extends State<ListenView> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: <Widget>[
+      Image.network('https://images.unsplash.com/photo-1670181741093-6503d030a0c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80'),
+      Container(child: Text("Stuff")),
+    ]);
   }
 }
